41b580f42c0f61eb4fc5bdf7884588aeeeef5b54
Niko-Bellic-69 <blankesx@gmail.com>
2025-08-03 23:38:17 +0530

    Update x.sh

--- x.sh
+++ x.sh
@@ -4,6 +4,8 @@ rm -rf .repo/local_manifests; \
 
 repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs; \
 
+/opt/crave/resync.sh; \
+
 rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf kernel/xiaomi/sm8250 && rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi; \
 
 git clone https://github.com/Niko-Bellic-69/dxm device/xiaomi/munch; \
@@ -13,7 +15,7 @@ git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_munch vendor/x
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi; \
 
-git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys && cd vendor/evolution-priv/keys && ./keys.sh && croot; \
+rm -rf vendor/evolution-priv && git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys && cd vendor/evolution-priv/keys && ./keys.sh && cd ../../..; \
 
 . build/envsetup.sh; \
 
