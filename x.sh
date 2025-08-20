02dc0a3a7be04ce75153956b80fa3073da015f31
Niko-Bellic-69 <blankesx@gmail.com>
2025-08-20 15:07:28 +0530

    Update x.sh

--- x.sh
+++ x.sh
@@ -2,7 +2,7 @@
 
 rm -rf .repo/local_manifests; \
 
-repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle; \
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs; \
 
 /opt/crave/resync.sh; \
 
@@ -13,8 +13,8 @@ git clone https://github.com/LineageOS/android_device_xiaomi_sm8250-common devic
 git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8250 kernel/xiaomi/sm8250; \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_munch vendor/xiaomi/munch; \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi; \
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi; \
 
 . build/envsetup.sh; \
 
-brunch munch
+lunch lineage_munch-bp2a-user && m evolution
