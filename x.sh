12f9d2ff875a72b21d48abb6fde336c22a4ef426
Niko-Bellic-69 <blankesx@gmail.com>
2025-09-13 19:09:38 +0530

    Update x.sh

--- x.sh
+++ x.sh
@@ -2,13 +2,13 @@
 
 rm -rf .repo/local_manifests; \
 
-repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs; \
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle; \
 
 /opt/crave/resync.sh; \
 
 rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf kernel/xiaomi/sm8250 && rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi; \
 
-git clone https://github.com/Niko-Bellic-69/dxm device/xiaomi/munch; \
+git clone https://github.com/LineageOS/android_device_xiaomi_munch device/xiaomi/munch; \
 git clone https://github.com/LineageOS/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
 git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8250 kernel/xiaomi/sm8250; \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_munch vendor/xiaomi/munch; \
@@ -17,6 +17,4 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi; 
 
 . build/envsetup.sh; \
 
-rm -rf vendor/evolution-priv/keys && git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys && cd vendor/evolution-priv/keys && ./keys.sh; \
-
-lunch lineage_munch-bp1a-user && m evolution
+brunch munch
