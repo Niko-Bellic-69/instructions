72845bdb023770380ac1d9bf96c8a30fc5d0cb18
Niko-Bellic-69 <blankesx@gmail.com>
2025-09-14 15:04:58 +0530

    Update x.sh

--- x.sh
+++ x.sh
@@ -9,7 +9,7 @@ repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --n
 rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf kernel/xiaomi/sm8250 && rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi; \
 
 git clone https://github.com/LineageOS/android_device_xiaomi_munch device/xiaomi/munch; \
-git clone https://github.com/LineageOS/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
+git clone https://github.com/Niko-Bellic-69/dxc device/xiaomi/sm8250-common; \
 git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8250 kernel/xiaomi/sm8250; \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_munch vendor/xiaomi/munch; \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
