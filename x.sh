bd1fb8ca62dff47758317d989f52ef91dd99e84f
Niko-Bellic-69 <blankesx@gmail.com>
2025-09-22 21:36:13 +0530

    Update x.sh

--- x.sh
+++ x.sh
@@ -2,22 +2,19 @@
 
 rm -rf .repo/local_manifests; \
 
-repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle; \
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs; \
 
 /opt/crave/resync.sh; \
 
 rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf kernel/xiaomi/sm8250 && rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi; \
 
-git clone https://github.com/LineageOS/android_device_xiaomi_munch device/xiaomi/munch; \
+git clone https://github.com/Niko-Bellic-69/dxm device/xiaomi/munch; \
 git clone https://github.com/Niko-Bellic-69/dxc device/xiaomi/sm8250-common; \
 git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8250 kernel/xiaomi/sm8250; \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_munch vendor/xiaomi/munch; \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
 git clone https://github.com/crdroidandroid/android_hardware_xiaomi hardware/xiaomi; \
 
-rm -rf vendor/lineage; \
-git clone https://github.com/Niko-Bellic-69/avc vendor/lineage; \
-
 . build/envsetup.sh; \
 
-brunch munch
+gk -s && axion munch && axionSync && ax -br
