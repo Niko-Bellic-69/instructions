50040f4bab20010a73a93a108436ab57b4d2587c
Niko-Bellic-69 <blankesx@gmail.com>
2025-09-23 16:33:41 +0530

    Update x.sh

--- x.sh
+++ x.sh
@@ -12,7 +12,7 @@ git clone https://github.com/Niko-Bellic-69/dxm device/xiaomi/munch; \
 git clone https://github.com/Niko-Bellic-69/dxc device/xiaomi/sm8250-common; \
 git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8250 kernel/xiaomi/sm8250; \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_munch vendor/xiaomi/munch; \
-git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
+git clone https://github.com/Niko-Bellic-69/vxc vendor/xiaomi/sm8250-common; \
 git clone https://github.com/crdroidandroid/android_hardware_xiaomi hardware/xiaomi; \
 
 . build/envsetup.sh; \
